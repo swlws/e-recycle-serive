32460e4ba895773a313a63ba59a59fb374263306
swlws <979623074@qq.com>
2025-04-25 09:17:59 +0800

    发送订阅消息

--- src/business/wx-openapi/send_subscribe_message.ts
+++ src/business/wx-openapi/send_subscribe_message.ts
@@ -1,10 +1,69 @@
 import { send_post } from "../../lib/http";
 
 export enum TemplateId {
-  ORDER_STATUS = "TEMPLATE_ID_1",
-  DELIVERY_UPDATE = "TEMPLATE_ID_2",
-  // TODO: 添加更多模板
+  /** 当师傅接单 */
+  WHEN_USER_TAKE_TASK = "zx06aI1Gj2s4UESFqwMgcXUK7Ck5wPN_OyDE8xokM0s",
+  /** 当师傅取消订单 */
+  WHEN_USER_UNTAKE_TASK = "RgYL3cHvNJXFjDCWR_joPDx-7k7QnYuejQ7HR2khHNc",
+  /** 当发布者删除任务 */
+  when_delete_task = "TV8NHyuOumNGad-rX-LqzvvUfzsr491aXKKjgvUlrww",
 }
+
+/** 消息体 - 接单 */
+export type WhenUserTakeTaskBody = {
+  // 订单编号
+  character_string1: {
+    DATA: string;
+  };
+  // 师傅姓名
+  name3: {
+    DATA: string;
+  };
+  // 师傅电话
+  phone_number6: {
+    DATA: string;
+  };
+  // 温馨提示
+  thing11: {
+    DATA: string;
+  };
+};
+
+/** 消息体 - 取消师傅任务 */
+export type WhenUserUntakeTaskBody = {
+  // 订单编号
+  character_string1: {
+    DATA: string;
+  };
+  // 师傅姓名
+  thing4: {
+    DATA: string;
+  };
+  // 温馨提示
+  thing5: {
+    DATA: string;
+  };
+};
+
+/** 消息体 - 发布者删除任务 */
+export type WhenDeleteTaskBody = {
+  // 工单编号
+  character_string33: {
+    DATA: string;
+  };
+  // 原服务时间
+  date7: {
+    DATA: string;
+  };
+  // 取消时间
+  date8: {
+    DATA: string;
+  };
+  // 取消理由
+  thing6: {
+    DATA: string;
+  };
+};
 
 export interface SendSubscribeMessageBody {
   // 用户的openid
@@ -14,11 +73,17 @@ export interface SendSubscribeMessageBody {
   // 点击模板卡片后的跳转页面，仅限本小程序内的页面。支持带参数,（示例index?foo=bar）。该字段不填则模板无跳转。
   page?: string;
   // 模板内容，格式形如 { "key1": { "value": any }, "key2": { "value": any } }
-  data: Record<string, { value: string }>;
+  data: WhenUserTakeTaskBody | WhenUserUntakeTaskBody | WhenDeleteTaskBody;
   // 跳转小程序类型：developer为开发版；trial为体验版；formal为正式版；默认为正式版
   miniprogram_state?: "developer" | "trial" | "formal";
 }
 
+/**
+ * 发送订阅消息
+ * @param access_token
+ * @param params
+ * @returns
+ */
 export function send_subscribe_message(
   access_token: string,
   params: SendSubscribeMessageBody
