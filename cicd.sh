4157a0b1a469e6f4fef91a2dd68974ab11b15eb2
swlws <979623074@qq.com>
2025-04-27 16:58:13 +0800

    sh

--- cicd.sh
+++
@@ -1,6 +0,0 @@
-# /usr/bin/sh bash
-
-pnpm tsc
-zip -r dist.zip ./dist
-scp ./dist.zip root@119.91.217.77:/root/dist.zip
-rm -rf ./dist.zip
--- publish.sh
+++
@@ -1,7 +0,0 @@
-# /usr/bin/sh bash
-
-rm -rf /root/swlws/platform-be/dist*
-unzip /root/dist.zip -d /root/swlws/platform-be
-rm -rf dist.zip
-pm2 restart all
-pm2 list
