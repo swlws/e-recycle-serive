0d7e5e2750d699c969560357b4f26dbad506f88f
swlws <979623074@qq.com>
2025-05-04 08:39:06 +0800

    定时任务

--- src/corn/auto_finish_task.ts
+++ src/corn/auto_finish_task.ts
@@ -5,18 +5,18 @@ import logger from "../lib/logger";
 import { getNowYMD, getNowYMDHMS } from "../tools/time";
 import { batch_insert_log } from "../business/log/insert_log";
 
-export async function check_invalid_task() {
+export async function auto_finish_task() {
   try {
     const mongo = await getMongo();
 
     // 查找并更新所有执行时间大于当天时间的任务
     const query = {
       pickupTime: { $lt: getNowYMD() + " 23:59:59" },
-      state: { $eq: ENUM_TASK_STATE.PENDDING },
+      state: { $eq: ENUM_TASK_STATE.WILL_RESOLVE },
     };
 
     const update = {
-      state: ENUM_TASK_STATE.INVALID,
+      state: ENUM_TASK_STATE.RESOLVE,
       updateTime: getNowYMDHMS(),
     };
 
@@ -31,10 +31,10 @@ export async function check_invalid_task() {
 
     await batch_insert_log({
       type: "system",
-      content: `成功将 ${modifiedCount} 个过期任务设置为无效状态`,
+      content: `自动完成 ${modifiedCount} 个任务`,
     });
-    logger.info(`成功将 ${modifiedCount} 个过期任务设置为无效状态`);
+    logger.info(`自动完成 ${modifiedCount} 个任务`);
   } catch (error) {
-    logger.error("更新任务状态时发生错误:", error);
+    logger.error("自动完成任务时发生错误:", error);
   }
 }
--- src/corn/index.ts
+++ src/corn/index.ts
@@ -1,13 +1,18 @@
 import { CronJob } from "cron";
 import { check_invalid_task } from "./check_invalid_task";
+import { auto_finish_task } from "./auto_finish_task";
 
 /**
  * 注册任务：任务的执行中状态同步
  */
 function register_task_doing_state_sync() {
-  // 每五分钟执行一次
-  const job = new CronJob("*/5 * * * *", check_invalid_task);
-  job.start();
+  // 检测过期任务 - 每日00点01分触发
+  const checkInvalidTaskJob = new CronJob("01 00 * * *", check_invalid_task);
+  checkInvalidTaskJob.start();
+
+  // 自动完成任务 - 每日00点02分触发
+  const autoFinishTaskJob = new CronJob("02 00 * * *", auto_finish_task);
+  autoFinishTaskJob.start();
 }
 
 register_task_doing_state_sync();
